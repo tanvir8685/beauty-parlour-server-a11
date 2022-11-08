56dd526b4c4f9f9f2126f630a24d63fcf66dd161
tanvir <tanvirhridoy1011@gmail.com>
2022-11-09 01:34:15 +0600

    mongo db set up with env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app=express();
 require('dotenv').config();
 const port=process.env.PORT||5000;
@@ -9,6 +10,19 @@ const port=process.env.PORT||5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+console.log(process.env.DB_USER)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ygtywwo.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 
 app.get('/',(req,res)=>{
