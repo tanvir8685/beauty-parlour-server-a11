4dce1a1a49c8da52a3ff29f419d4ab5a1d033065
tanvir <tanvirhridoy1011@gmail.com>
2022-11-09 02:59:50 +0600

    makeing service api

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const app=express();
+const app = express();
 require('dotenv').config();
-const port=process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 
 
 
@@ -18,12 +18,33 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const serviceCollection = client.db('beauty-parlour-db').collection('services');
+        app.get('/', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services)
+        })
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
 
+    }
+    finally {
 
+    }
 
-app.get('/',(req,res)=>{
+}
+
+run().catch(err => console.error(err))
+app.get('/', (req, res) => {
     res.send('this is working')
 })
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Genious car server running port ${port}`)
 })
