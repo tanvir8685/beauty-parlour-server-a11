17817cfc7a87325a823bdd25e7deb41bed2012dd
tanvir <tanvirhridoy1011@gmail.com>
2022-11-09 01:44:55 +0600

    mongo work

--- index.js
+++ index.js
@@ -15,13 +15,9 @@ app.use(express.json());
 console.log(process.env.DB_USER)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ygtywwo.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
 
 
 
